2b51347b42791a32ba24af57bd0fa9a1c76abf82
Marco-Sahagun <Marcosahagun751@gmail.com>
2024-06-17 17:41:29 -0700

    Add files via upload

---
+++ AttitudeMathLib/example/main.cpp
@@ -0,0 +1,17 @@
+// System Includes
+#include <iostream>
+#include <string>
+#include <cmath>
+
+//Program includes
+#include "AML.h"
+using namespace AML;
+
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+
+}
+
