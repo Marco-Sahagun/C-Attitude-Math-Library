ab68e71a5037967f04108cc814bd39291453ad80
Marco-Sahagun <Marcosahagun751@gmail.com>
2024-12-19 12:18:28 -0800

    Update AMLMatrix33.cpp

--- AML/AMLMatrix33.cpp
+++ AML/AMLMatrix33.cpp
@@ -1,9 +1,3 @@
-// ######################################################################### //
-// Attitude Math Library
-// Copyright (c) 2021 Dr. Steven Dumble Phd
-// Distributed under the MIT Software License (See accompanying file LICENSE)
-// ######################################################################### //
-
 #include "AMLMatrix33.hpp"
 #include "AMLVector3.hpp"
 #include <cmath>
