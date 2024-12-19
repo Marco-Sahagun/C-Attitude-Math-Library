fe01432a0599b674d4fac007ee14b838aad41183
Marco-Sahagun <Marcosahagun751@gmail.com>
2024-12-19 12:18:40 -0800

    Update AMLMatrix33.hpp

--- AML/AMLMatrix33.hpp
+++ AML/AMLMatrix33.hpp
@@ -1,9 +1,3 @@
-// ######################################################################### //
-// Attitude Math Library
-// Copyright (c) 2021 Dr. Steven Dumble Phd
-// Distributed under the MIT Software License (See accompanying file LICENSE)
-// ######################################################################### //
-
 #ifndef AML_AMLMATRIX33_H
 #define AML_AMLMATRIX33_H
 
